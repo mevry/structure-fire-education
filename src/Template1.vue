8a40d1177898d27509fcd394d9aa4d4efa723f08
Derek Miller <derekjmiller@gmail.com>
2018-04-23 10:36:32 -0700

    create team member folders

--- src/Template1.vue
+++ src/Template1.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-          <h1>This is a dynamic component</h1>
+          <h1>This is a dynamic component omg lolz 123</h1>
           <button type="button" class="btn btn-primary">A button</button>
   </div>
 </template>
