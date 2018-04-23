28d43d45abfffba7774c4e3e1f50de9318f40310
Derek Miller <derekjmiller@gmail.com>
2018-04-23 10:25:13 -0700

    added single file component structure

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+<button class="btn btn-success" v-if="templateType == 'template-one'" @click="templateType = 'template-two'">Change Template Type</button>
+<button class="btn btn-success" v-else @click="templateType = 'template-one'">Change Template Type</button>
+<br>
+<component :is="templateType"></component>
+
+ </div>
+</template>
+
+<script>
+import TemplateOne from './Template1.vue'
+import TemplateTwo from './Template2.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      templateType:"template-one"
+    }
+  },
+  components:{
+    'template-one':TemplateOne,
+    'template-two':TemplateTwo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Template1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+          <h1>This is a dynamic component</h1>
+          <button type="button" class="btn btn-primary">A button</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'template-one',
+  data (){
+    return {
+      "container-type":"fluid"
+    }
+  }
+};
+</script>
+<style scoped>
+  .container,.container-fluid{
+    background-color: red;
+  }
+</style>
