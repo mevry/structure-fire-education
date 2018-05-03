a86d240fc265203771b0246de776f93ac02fc132
Derek Miller <derekjmiller@gmail.com>
2018-05-02 23:15:57 -0700

    added basic jeopardy
    added enough functionality for demonstration

--- src/components/Jeopardy.vue
+++ src/components/Jeopardy.vue
@@ -1,21 +1,187 @@
 <template>
   <div class="container-fluid">
+      <jeopardy-modal v-if="showJeopardyModal" :questionObject="currentQuestion" :pointValue="currentQuestionPV" @submission="showJeopardyModal = false">
 
+      </jeopardy-modal>
       <h1>Jeopardy Component</h1>
-      <p>Copying and pasting</p>
-      <p>{{message}}</p>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Home Safety</th>
+            <th>Fire Concepts</th>
+            <th>Outdoor Safety</th>
+            <th>Firefighting Methods</th>
+            <th>Emergency Cases</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td @click="renderJeopardyModal('columnOne',0)">100</td>
+            <td>100</td>
+            <td>100</td>
+            <td>100</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td @click="renderJeopardyModal('columnOne',1)">200</td>
+            <td>200</td>
+            <td>200</td>
+            <td>200</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td @click="renderJeopardyModal('columnOne',2)">300</td>
+            <td>300</td>
+            <td>300</td>
+            <td>300</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td @click="renderJeopardyModal('columnOne',3)">400</td>
+            <td>400</td>
+            <td>400</td>
+            <td>400</td>
+            <td>400</td>
+          </tr>
+          <tr>
+            <td @click="renderJeopardyModal('columnOne',4)">500</td>
+            <td>500</td>
+            <td>500</td>
+            <td>500</td>
+            <td>500</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 @submission="updatePoints()">Current Points: {{score}}</h2>
   </div>
 </template>
 <script>
-
-
+import JeopardyModal from './JeopardyModal.vue'
+import EventBus from '../event-bus'
+import store from '../store'
 
 export default {
   name: 'jeopardy',
   data(){
     return{
-      message:"this is a message"
+      showJeopardyModal:false,
+      currentQuestion:{},
+      currentQuestionPV:0,
+      score:0,
+      sharedState:store.state,
+      //order by difficulty
+      //types: tf,multi,fill
+      questionBank:{
+        columnOne:[
+          {
+            type:"tf",
+            question:"Your home should have a smoke detector to warn you in case of a smoke emergency.",
+            answer:[true],
+          },
+          {
+            type:"multi",
+            question:"This will allow you to escape from 2nd story houses.",
+            answer:[3],
+            options:[
+              {
+                key:0,
+                value:"Trampoline"
+              },
+              {
+                key:1,
+                value:"Parachute"
+              
+              },
+              {
+                key:2,
+                value:"Giant Drone"
+              },
+              {
+                key:3,
+                value:"Rope Ladder"
+              }             
+            ]
+          },
+          {
+            type:"tf",
+            question:"You should have less than two escape routes per room.",
+            answer:[false]
+          },
+          {
+            type:"fill",
+            question:{
+              partOne:"You should practice a family escape plan",
+              partTwo:"times per year.",
+            },
+            answer:["two", 2]
+          },
+          {
+            type:"multi",
+            question:"Your family should use this to gather together after a home fire.",
+            answer:[0],
+            options:[
+              {
+                key:0,
+                value:"A Meeting Place"
+              },
+              {
+                key:1,
+                value:"Sushi"
+              
+              },
+              {
+                key:2,
+                value:"Smoke Detector"
+              },
+              {
+                key:3,
+                value:"Rope Ladder"
+              }             
+            ]
+          }
+        ],
+        columnTwo:[],
+        columnThree:[],
+        columnFour:[],
+        columnFive:[]
+      }
+      
     }
+  },
+  methods:{
+    renderJeopardyModal(col,pos){
+      this.currentQuestion = this.questionBank[col][pos];
+      this.updateQuestionPV(pos);
+      this.showJeopardyModal = true;
+    },
+    updateQuestionPV(position){
+     this.currentQuestionPV = (position+1)*100;
+    },
+    updatePoints(correct){
+      this.showJeopardyModal = false
+      console.log("current point val: " + this.currentQuestionPV)
+      if(correct){       
+        this.score += this.currentQuestionPV;
+      }
+      this.checkJeopardyComplete();
+    },
+    checkJeopardyComplete(){
+      if(this.score > 900){
+        this.sharedState.JeopardyScore = this.score;
+        this.emitMethod('activity-completed', "Jeopardy");
+        console.log('Jeopardy Completed');
+        
+      }
+    },
+    emitMethod(evt,payload){
+      EventBus.$emit(evt,payload);
+    }
+  },
+  components:{
+    'jeopardy-modal':JeopardyModal
+  },
+  mounted(){
+    EventBus.$on('submission', this.updatePoints);
   }
 };
 </script>
---
+++ src/components/JeopardyModal.vue
@@ -0,0 +1,185 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+            <form @submit.prevent>
+                <div class="true-false" v-if="questionObject.type == 'tf'">
+                    <div class="modal-header">
+                        <h3>True / False</h3><br>
+                        
+                    </div>
+                    <div class="modal-body">
+                        <h4>{{questionObject.question}}</h4><br>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <label for="" class="form-check-label">
+                                    <input type="radio" name="tf" value="1" class="form-check-input" v-model.number="tfAnswer"> True
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <label for="" class="form-check-label">
+                                    <input type="radio" name="tf" value="0"  class="form-check-input" v-model="tfAnswer"> False
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--End true-false-->
+                <div class="multi" v-else-if="questionObject.type == 'multi'">
+                    <div class="modal-header">
+                        <h3>Multiple Choice</h3><br>                   
+                    </div>
+                    <div class="modal-body">
+                        <h4>{{questionObject.question}}</h4><br>
+                        <div class="form-group">
+    
+                            <div v-for="option in questionObject.options" :key="option.key" class="form-check">
+                                <label for="" class="form-check-label">
+                                    <input type="radio" name="multi" v-bind:value="option.key" class="form-check-input" v-model.number="multiAnswer"> {{option.value}}
+                                </label>
+                            </div>
+    
+                        </div>
+                    </div>
+    
+                </div><!--End multi-->
+                <div class="fill" v-else-if="questionObject.type == 'fill'">
+                    <div class="modal-header">
+                        <h3>Multiple Choice</h3><br>                   
+                    </div>
+                    <div class="modal-body">
+                        <h4>{{questionObject.question.partOne}}</h4><input type="text" name="fill" v-model.lazy="fillAnswer"><h4>{{questionObject.question.partTwo}}</h4>
+                    </div>
+                </div><!--End fill-->
+    
+                <div class="modal-footer">
+                    <p>{{pointValue}} Point Question</p>
+                    <slot name="footer">
+                        <button class="modal-default-button" @click="checkAnswer">
+                        SUBMIT
+                        </button>
+                    </slot>
+                </div>
+            </form>
+
+        </div><!--modal-container-->
+      </div><!--modal-wrapper-->
+    </div><!--modal-mask-->
+  </transition>
+</template>
+<script>
+import EventBus from '../event-bus'
+export default {
+  name:"jeopardy-modal",
+  props:['questionObject', 'pointValue'],
+  data(){
+      return{
+          tfAnswer:null,
+          multiAnswer:null,
+          fillAnswer:''
+      }
+  },
+  computed:{
+      boolAnswer(){
+          if(this.tfAnswer == 0){
+              return false;
+          }else{
+              return true;
+          }
+      }
+  },
+  methods:{
+      checkAnswer(){
+          let correctAnswer = this.$props.questionObject.answer;
+          let questionType = this.$props.questionObject.type;
+            let userAnswer;
+          if(questionType == 'tf'){
+            userAnswer = this.boolAnswer
+          }else if(questionType == 'multi'){
+              userAnswer = this.multiAnswer
+          }else{
+              userAnswer = this.fillAnswer
+          }
+        
+          let found = correctAnswer.includes(
+              userAnswer        
+          );
+
+          console.log("Your answer: " + userAnswer + " " + typeof(userAnswer));
+          console.log("Correct answer: " + correctAnswer)
+          console.log("Comparison returns: " + found)
+           console.log(found ? "Answer Correct" : "Answer Incorrect");
+           this.emitMethod('submission',found);
+      },
+      emitMethod(evt,payload){
+          EventBus.$emit(evt,payload);
+      }
+  }
+}
+</script>
+<style lang="scss">
+  .modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 60%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
