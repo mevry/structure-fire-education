0e95a0e3a64db80b104dae2e72f9d23a865b7766
Derek Miller <derekjmiller@gmail.com>
2018-05-08 14:30:07 -0700

    added activity gifs to SiteMenu

--- src/components/SiteMenu.vue
+++ src/components/SiteMenu.vue
@@ -2,22 +2,22 @@
 <div class="container-fluid text-center py-3">
 <h3 class="neon-yellow">Select an activity below to get started</h3>
     <div class="d-flex justify-content-around mt-3 pt-3">
-        <div class="activity-item-wrapper">
-            <router-link to="/jeopardy" tag="div" class="activity-link mx-1">
-                <h1>Jeopardy</h1>
-                <img src="../img/jepBadge.png" />
+        <div class="mx-2">
+            <router-link to="/jeopardy" tag="div" class="activity-link mx-1" >
+                <!-- <h1>Jeopardy</h1> -->
+                <img class="img-fluid rounded shadow" style="border:4px solid #8c52c6;" src="../img/jeopardyattempt.gif" />
             </router-link>
         </div>
-        <div class="">
+        <div class="mx-2">
             <router-link to="/drag-drop" tag="div" class="activity-link mx-1">
-                <h1>Drag &amp; Match</h1>
-                <img src="../img/dragBadge.png" />
+                <!-- <h1>Drag &amp; Match</h1> -->
+                <img class="img-fluid rounded shadow" style="border:4px solid #cead49;" src="../img/dragattempt.gif" />
             </router-link>
         </div>
-        <div class="">
+        <div class="mx-2">
             <router-link to="/memory" tag="div" class="activity-link mx-1">
-                <h1>Memory</h1>
-                <img src="../img/memBadge.png" />
+                <!-- <h1>Memory</h1> -->
+                <img class="img-fluid rounded shadow" style="border:4px solid #689f4b;" src="../img/memoryattempt.gif" />
             </router-link>
         </div>
     </div>
@@ -36,6 +36,11 @@ export default {
 h1{
     color: white;
     margin-bottom: 0;
+}
+body {
+    color: #cead49;
+    color: #8c52c6;
+    color: #689f4b
 }
 </style>
 
